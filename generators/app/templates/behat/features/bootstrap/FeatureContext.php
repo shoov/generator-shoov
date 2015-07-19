b22828c74332b0e3c925364611228fe67e36fa30
Amitai Burstein <amitai@gizra.com>
2015-07-19 22:23:51 +0300

    Fix behat test

--- generators/app/templates/behat/features/bootstrap/FeatureContext.php
+++ generators/app/templates/behat/features/bootstrap/FeatureContext.php
@@ -12,6 +12,27 @@ class FeatureContext extends MinkContext implements SnippetAcceptingContext {
    * @Given I am an anonymous user
    */
   public function iAmAnAnonymousUser() {
-    // Sine Jekyll doesn't allow login, we just let this pass-through.
+    // Just let this pass-through.
+  }
+
+  /**
+   * @When I visit the homepage
+   */
+  public function iVisitTheHomepage() {
+    $this->getSession()->visit($this->locatePath('/'));
+  }
+
+  /**
+   * @Then I should have access to the page
+   */
+  public function iShouldHaveAccessToThePage() {
+    $this->assertSession()->statusCodeEquals('200');
+  }
+
+  /**
+   * @Then I should not have access to the page
+   */
+  public function iShouldNotHaveAccessToThePage() {
+    $this->assertSession()->statusCodeEquals('403');
   }
 }
