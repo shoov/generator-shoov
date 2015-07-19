1a50ad336cef1b3f983c3a9fa5730a46f158d3a4
nader77 <nader@gizra.com>
2015-07-19 14:22:14 +0300

    Fix file copying.

--- app/index.js
+++ app/index.js
@@ -1,5 +1,10 @@
 'use strict';
 var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var glob = require('glob');
+var fs = require('fs-extra');
+var path = require('path');
+var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
   initializing: function () {
@@ -9,7 +14,29 @@ module.exports = yeoman.generators.Base.extend({
   writing: {
     app: function() {
       var self = this;
-      glob.sync(self.templatePath() + '/**/*');
+
+      // Have Yeoman greet the user.
+      self.log(yosay(
+        'Welcome to the ' + chalk.red('Shoov') + ' generator!'
+      ));
+
+      var files  = glob.sync(self.templatePath() + '/**/*');
+
+      files.forEach(function(file) {
+        if (fs.lstatSync(file).isDirectory()) {
+          // Don't try to copy a directory.
+          return;
+        }
+
+        var fileName = file.replace(self.templatePath('/'), '');
+
+        var dir = path.dirname(fileName);
+        var baseName = path.basename(fileName);
+
+        var newFileName = dir ? dir + '/' + baseName : baseName;
+
+        self.fs.copy(self.templatePath(fileName), self.destinationPath(newFileName));
+      });
     }
   }
 });
