86673cf2b56e94fa4c326e282591d0809889f396
nader77 <nader@gizra.com>
2015-07-19 20:16:01 +0300

    First commit of the yo shoov generator

---
+++ app/index.js
@@ -0,0 +1,72 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var glob = require('glob');
+var fs = require('fs-extra');
+var path = require('path');
+var yosay = require('yosay');
+
+module.exports = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    var self = this;
+
+    // Have Yeoman greet the user.
+    self.log(yosay(
+      'Welcome to the ' + chalk.red('Shoov') + ' generator!'
+    ));
+
+    done();
+  },
+
+  writing: {
+    app: function() {
+      var self = this;
+
+      var files  = glob.sync(self.templatePath() + '/**/*');
+
+      files.forEach(function(file) {
+        if (fs.lstatSync(file).isDirectory()) {
+          // Don't try to copy a directory.
+          return;
+        }
+
+        var fileName = file.replace(self.templatePath(), '');
+
+        var dir = path.dirname(fileName);
+        var baseName = path.basename(fileName);
+        var extension = path.extname(baseName);
+
+        if (extension !== '.scss') {
+          // If not a SCSS file, convert the prefix of the underscore to a dot.
+          baseName = baseName.replace(/^_/g, '.');
+        }
+
+        var newFileName = dir != '/' ? dir.replace('/', '') + '/' + baseName : baseName;
+
+        self.fs.copy(file, self.destinationPath(newFileName));
+      });
+    }
+  },
+
+  install: {
+
+    /**
+     * Install npm.
+     */
+    client: function() {
+      if (this.options['skip-install']) {
+        this.log('Skipping install');
+        return;
+      }
+
+      this.log('npm install');
+      this.npmInstall(null, {cwd: 'visual-monitor'});
+    }
+  }
+});
